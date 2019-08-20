1f59ce63b191af5bd452cebf78befae91efe844e
Levi Borland <borland.levi@gmail.com>
2019-08-20 02:42:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -98,12 +98,15 @@ end
 all_supplies_in_holidays(holiday_supplies)
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.map do |season, holiday|
-    holiday.map do |holiday, supply|
-      holiday if supply.include?("BBQ")
+  # return an array of holiday names (as symbols) where supply lists
+  # include the string "BBQ"
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
     end
   end.flatten.compact
 end
+all_holidays_with_bbq(holiday_supplies)
 
 
 
